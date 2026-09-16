1095a9227cf9b71705413ce8eb2115c107980abe
Muhammad Usman <151637196+MuhammadUsmanGulzar@users.noreply.github.com>
2026-09-17 01:08:01 +0500

    Fix Vercel deployment configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-    distDir: process.env.PORTFOLIO_BUILD_DIR || '.next',
 };
 
 export default nextConfig;
-
